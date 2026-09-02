102bc1f05ca2e29b339bfc3a13c9865b992b2cf0
djahern-max <danielaherniv@gmail.com>
2026-09-02 19:25:25 -0400

    deleting everything and starting fresh

--- remotion.config.ts
+++
@@ -1,7 +0,0 @@
-import { Config } from "@remotion/cli/config";
-
-Config.setVideoImageFormat("jpeg");
-Config.setOverwriteOutput(true);
-// CRF 18 is visually lossless enough for slides that are mostly type.
-// Type artefacts at higher CRF are the first thing a participant notices.
-Config.setCrf(18);
