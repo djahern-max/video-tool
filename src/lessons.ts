e14fb08cdacc5d425aafb7adf0096ab37f809b14
djahern-max <danielaherniv@gmail.com>
2026-09-02 19:32:17 -0400

    removed lessons from lesson.ts

--- src/lessons.ts
+++ src/lessons.ts
@@ -1,19 +1,7 @@
-import * as lesson01 from "./lesson-01";
-import * as lesson02 from "./lesson-02";
-import * as lesson03 from "./lesson-03";
-import * as lesson04 from "./lesson-04";
-import * as lesson05 from "./lesson-05";
-import * as lesson06 from "./lesson-06";
-import * as lesson99 from "./lesson-99";
+
 
 export const LESSONS = {
-  "01": lesson01,
-  "02": lesson02,
-  "03": lesson03,
-  "04": lesson04,
-  "05": lesson05,
-  "06": lesson06,
-  "99": lesson99,
+
 } as const;
 export type LessonId = keyof typeof LESSONS;
 
